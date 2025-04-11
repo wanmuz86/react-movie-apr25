2508d8e6409df17d284a5c107be8b6fc7a225983
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-11 14:06:02 +0700

    called api using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
+import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MovieList from './components/MovieList'
 import Search from './components/Search'
+import axios from 'axios'
 
 function App() {
-  const handleMovieSearch = (movieName) => {
+  const [movies, setMovies] = useState([])
+
+  const handleMovieSearch = async (movieName) => {
     console.log(movieName)
+
+    // Fetch movie data from API
+    const response = await axios.get(`https://www.omdbapi.com/?s=${movieName}&apikey=87d10179`)
+    console.log(response.data)
+    setMovies(response.data.Search)
   }
  
   return (
