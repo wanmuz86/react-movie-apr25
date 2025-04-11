eb6bd4f21cdeec58f0c748e89a13850a91c4899f
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-11 15:24:43 +0700

    add infitnit scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useState,useEffect,useRef } from 'react'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
@@ -12,14 +12,42 @@ import axios from 'axios'
 function App() {
   const [movies, setMovies] = useState([])
   const [movieId, setMovieId] = useState(null)
+  const [movieName, setMovieName] = useState('')  
+  const movieNameRef = useRef('');
 
-  const handleMovieSearch = async (movieName) => {
-    console.log(movieName)
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight;
+       
+
+      if (isBottom) {
+        console.log("Reached the bottom of the page!");
+        // Do something, like loading more content
+        console.log(movieName)
+        fetchApi(movieNameRef.current,2)
+      }
+    };
+
+    // When the window arrive at the end of the page, call the handleScroll function
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleMovieSearch = async (title) => {
+    
+    setMovieName(title)
+    movieNameRef.current = title; 
+    fetchApi(title,1)
 
     // Fetch movie data from API
-    const response = await axios.get(`https://www.omdbapi.com/?s=${movieName}&apikey=87d10179`)
-    console.log(response.data)
-    setMovies(response.data.Search)
+  
+  }
+
+  const fetchApi = async (movieName,page) => {
+    console.log(movieName)
+    const response = await axios.get(`https://www.omdbapi.com/?s=${movieName}&page=${page}&apikey=87d10179`)
+    setMovies(prev => [...prev, ...response.data.Search]);
   }
 
   const handlePassMovie = (imbdId) => {
